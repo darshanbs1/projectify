d66c36ec597a5586a48434529d9bebf31a3ebfe7
darshanbs1 <89129495+darshanbs1@users.noreply.github.com>
2022-11-06 21:33:50 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from flask import Flask,render_template, url_for
+from flask import Flask,render_template, url_for,flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 
 
 app=Flask(__name__)
-db = SQLAlchemy(app)
+
 app.config["SQLALCHEMY_DATABASE_URI"]='sqlite:///database.db'
 app.config['SECRET_KEY']='secretkey'
+db = SQLAlchemy(app)
+
 
 
 class User(db.Model,UserMixin):
-    id=db.column(db.Integer,primary_key=True)
+    id=db.Column(db.Integer,primary_key=True)
     username=db.Column(db.String(20),nullable=False)
     password=db.Column(db.String(80),nullable=False)
 
